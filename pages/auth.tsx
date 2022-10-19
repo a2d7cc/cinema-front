b8de5b508a7e9d4719b6d9a948b93b9953b6959e
a2d7cc <a2d7cc@gmail.com>
2022-10-19 16:15:39 +0200

    Form Auth(UI Fields, button)

---
+++ pages/auth.tsx
@@ -0,0 +1,11 @@
+import Auth from '@/components/screens/auth/Auth'
+import { NextPage } from 'next'
+import React, { FC } from 'react'
+
+const AuthPage: NextPage = () => {
+	return (
+		<Auth />
+	)
+}
+
+export default AuthPage
